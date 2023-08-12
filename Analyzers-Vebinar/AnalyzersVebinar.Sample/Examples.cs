5c37f1018cb08c536db57e6d9fdc2a81947b9e7b
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2023-08-12 22:48:31 +0300

    Init

---
+++ Analyzers-Vebinar/AnalyzersVebinar.Sample/Examples.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
+namespace AnalyzersVebinar.Sample;
+
+// If you don't see warnings, build the Analyzers Project.
+
+public class Examples
+{
+    public class MyCompanyClass
+    {
+    }
+
+    public void ToStars(Spaceship spaceship)
+    {
+        if (spaceship != null)
+        {
+            spaceship.SetSpeed(300000000);
+            spaceship.SetSpeed(42);
+        }
+    }
+}

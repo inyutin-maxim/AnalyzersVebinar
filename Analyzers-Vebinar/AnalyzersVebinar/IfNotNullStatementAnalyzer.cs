3e59d3e6c57efc0b41eb0ac79533e4b81b49a086
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2023-08-12 23:25:30 +0300

    Добавляет код анализа

--- Analyzers-Vebinar/AnalyzersVebinar/IfNotNullStatementAnalyzer.cs
+++ Analyzers-Vebinar/AnalyzersVebinar/IfNotNullStatementAnalyzer.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace AnalyzersVebinar;
 
+/// <inheritdoc />
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public class IfNotNullStatementAnalyzer : DiagnosticAnalyzer
 {
@@ -14,13 +17,23 @@ public class IfNotNullStatementAnalyzer : DiagnosticAnalyzer
 		context.EnableConcurrentExecution();
 		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze|GeneratedCodeAnalysisFlags.ReportDiagnostics);
 
-		throw new NotImplementedException();
+		context.RegisterCompilationStartAction(compilationContext =>
+		{
+			compilationContext.RegisterSyntaxNodeAction(analysisContext =>
+			{
+				if (analysisContext.Node.ChildNodes()
+					.Any(x => x.IsKind(SyntaxKind.NotEqualsExpression)))
+				{
+					analysisContext.ReportDiagnostic(Diagnostic.Create(GetSupportedDiagnostic(), analysisContext.Node.GetLocation()));
+				}
+			}, SyntaxKind.IfStatement);
+		});
 	}
 
 	/// <inheritdoc />
-	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = new()
-	{
-		new("Demo000001", "Проверка выроажение != null должно происходить через выражение is not",
-			"Проверка выроажение != null должно происходить через выражение is not", "CodeSyle", DiagnosticSeverity.Warning, true)
-	};
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =ImmutableArray.Create(GetSupportedDiagnostic());
+
+	private static DiagnosticDescriptor GetSupportedDiagnostic() => new("Demo000001",
+		"Проверка выражения != null должно происходить через выражение is not",
+		"Проверка выражения != null должно происходить через выражение is not", "CodeStyle", DiagnosticSeverity.Warning, true);
 }

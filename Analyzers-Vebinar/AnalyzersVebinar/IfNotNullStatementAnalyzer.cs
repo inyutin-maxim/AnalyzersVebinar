288988f6623bd0a141c43b18cd9c4a31129ba226
Инютин Максим Николаевич <inyutin_maxim@mail.ru>
2023-08-12 22:53:45 +0300

    Создает класс анализатора

---
+++ Analyzers-Vebinar/AnalyzersVebinar/IfNotNullStatementAnalyzer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace AnalyzersVebinar;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class IfNotNullStatementAnalyzer : DiagnosticAnalyzer
+{
+	/// <inheritdoc />
+	public override void Initialize(AnalysisContext context)
+	{
+		context.EnableConcurrentExecution();
+		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze|GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+		throw new NotImplementedException();
+	}
+
+	/// <inheritdoc />
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = new()
+	{
+		new("Demo000001", "Проверка выроажение != null должно происходить через выражение is not",
+			"Проверка выроажение != null должно происходить через выражение is not", "CodeSyle", DiagnosticSeverity.Warning, true)
+	};
+}
